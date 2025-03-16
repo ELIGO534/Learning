e2520dc01dd201c9957c81265bb1cccea002e186
eligo <eligorenderforest@gmail.com>
2025-03-16 22:20:35 +0530

    Updated urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,7 +6,7 @@ from django.urls import path
 from myapp import views
 
 urlpatterns = [
-    path('',views.login, name='login'),
+    path('',views.home, name='home'),
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('home/', views.home, name='home'),
